1d3baa6ca75e9ae01ff33303d224102b471d8f89
Sudhanshu Pandey <sudhanshu.pandey@successive.tech>
2023-10-15 00:01:45 +0530

    errorhandler fix

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-import errorHandler from "../utils/error.js";
+import {errorHandler} from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
@@ -23,7 +23,7 @@ export const signin = async (req, res, next) => {
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credential!"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const {password: pass, ...rest} = validUser._doc;
+    const { password: pass, ...rest } = validUser._doc;
     res
       .cookie("acess_token", token, {
         httpOnly: true,
