215008c827f1cf882255dcb5d1b9b23fa4e8a2eb
Sudhanshu Pandey <sudhanshu.pandey@successive.tech>
2023-10-15 18:39:46 +0530

    minor user name change

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -55,12 +55,10 @@ export const google = async (req, res, next) => {
         Math.random().toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
       const newUser = new User({
-        username:
-          req.body.name.split(" ").join("").toLowerCase() +
-          Math.random().toString(36).slice(-4),
+        username: req.body.name.split(" ").join("_").toLowerCase(),
         email: req.body.email,
         password: hashedPassword,
-        avatar: req.body.photo
+        avatar: req.body.photo,
       });
       await newUser.save();
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
