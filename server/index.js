1fb3dffba9f5b880a167001ebea90ef6e7414457
Sudhanshu Pandey <sudhanshu.pandey@successive.tech>
2023-10-15 21:35:10 +0530

    add create listing api route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -28,6 +29,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 //error-handling middleware
 app.use((err, req, res, next) => {
