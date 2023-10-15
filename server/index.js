40de03efca3905737f869089452638da6ddd5667
Sudhanshu Pandey <sudhanshu.pandey@successive.tech>
2023-10-15 13:56:17 +0530

    create update user api route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -18,6 +19,8 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log("server is running on port 3000");
