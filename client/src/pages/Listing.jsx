ff4c34c53c2271a0de05055cf22f38b35847dd74
Sudhanshu Pandey <sudhanshu.pandey@successive.tech>
2023-10-22 16:50:48 +0530

    add contact landlord functionality to the listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -14,6 +14,7 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
+import Contact from '../components/Contact';
 
 const Listing = () => {
   SwiperCore.use([Navigation]); //Navigation is a Swiper module that provides navigation arrows
@@ -21,6 +22,7 @@ const Listing = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false);
   const params = useParams();
   const { currentUser } = useSelector((state) => state.user);
 
@@ -44,8 +46,6 @@ const Listing = () => {
       }
     };
     fetchListing();
-    console.log(currentUser);
-    console.log(listing.userRef);
   }, [params.listingId]);
 
   return (
@@ -134,6 +134,15 @@ const Listing = () => {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+              >
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -36,24 +36,38 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-export const deleteUser = async(req, res, next) => {
-  if(req.user.id != req.params.id) return next(errorHandler(401, 'You can only delete your own account!'));
-  try{
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id != req.params.id)
+    return next(errorHandler(401, "You can only delete your own account!"));
+  try {
     await User.findByIdAndDelete(req.params.id);
     res.clearCookie("access_token");
-    res.status(200).json('User has been deleted!');
-  }catch(error){
+    res.status(200).json("User has been deleted!");
+  } catch (error) {
     next(error);
   }
-}
+};
 
-export const getUserListing = async(req, res, next) => {
-  if(req.user.id != req.params.id) return next(errorHandler(401, 'You can only view your own listing!'));
-  try{
-    const listing = await Listing.find({ userRef: req.params.id});
+export const getUserListing = async (req, res, next) => {
+  if (req.user.id != req.params.id)
+    return next(errorHandler(401, "You can only view your own listing!"));
+  try {
+    const listing = await Listing.find({ userRef: req.params.id });
     res.status(200).json(listing);
-  }catch(error){
+  } catch (error) {
     next(error);
   }
+};
 
-}
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) return next(errorHandler(404, "User not found"));
+
+    const { password: pass, ...rest } = user._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
