c8379feb2c620dc40a28e591c26b15062f24d826
Sudhanshu Pandey <sudhanshu.pandey@successive.tech>
2023-10-22 11:25:03 +0530

    create update listing api route

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -1,25 +1,45 @@
 import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
-export const createListing = async(req, res, next) => {
-    try{
-        const listing = await Listing.create(req.body);
-        return res.status(200).json(listing);
-    }catch(error){
-        next(error);
-    }
-}
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteListing = async(req, res, next) => {
-    const listing = await Listing.findById(req.params.id);
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
 
-    if(!listing) return next(errorHandler(404, 'Listing not found!'));
-    if(req.user.id !== listing.userRef) return next(errorHandler(401, 'You can only delete your own listing!'));
+  if (!listing) return next(errorHandler(404, "Listing not found!"));
+  if (req.user.id !== listing.userRef)
+    return next(errorHandler(401, "You can only delete your own listing!"));
 
-    try{
-        await Listing.findByIdAndDelete(req.param.id);
-        res.status(200).json('Listing has been deleted!');
-    }catch(error){
-        next(error);
-    }
-}
+  try {
+    await Listing.findByIdAndDelete(req.param.id);
+    res.status(200).json("Listing has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) return next(errorHandler(404, "Listing not found!"));
+  if (req.user.id !== listing.userRef)
+    return next(errorHandler(401, "You can only update your own listing!"));
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true } // because we want to get new update listing
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
